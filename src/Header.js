573a2c4f4e10dfd19214167baa219cd2818bb12a
Vishal-m <vishalmahesh.study@gmail.com>
2023-04-06 20:33:32 +0530

    styled header

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 
 const Header = () => {
+    const style = {textAlign:'center', padding: 20, textTransform: 'uppercase', backgroundColor: 'black', color:'white'};
     return(
-        <div className='header'>
-            Phone Directory
+        <div style={style}>
+            <h1>Phone Directory</h1>
         </div>
     );
 }
